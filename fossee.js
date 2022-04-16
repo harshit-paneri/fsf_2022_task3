554968b79f6a19ed9ef9474a70e37eb6e5b2ed08
Harshit Paneri <paneriharshit29@gmail.com>
2022-04-16 19:54:10 +0530

    Fossee data

---
+++ fossee.js
@@ -0,0 +1,16 @@
+
+function promp(){
+  var jo=  alert("You will be  directed to a google form ");
+    
+    if(!jo){
+    window.location.href="https://docs.google.com/forms/d/e/1FAIpQLSfcGJSeLmKuOEEidUOJhKnDZSdcsPkSwWw3rQUtfR8LIRZugA/viewform?usp=sf_link"
+    }
+}
+function map(){
+  if(mapfossee){
+    x="https://goo.gl/maps/Yx7Q6y1MQfDvVMHu7";
+    
+    window.location.href=x;
+    
+  }
+}
